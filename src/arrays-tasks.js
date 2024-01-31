385cb56bca9d726f95c9f8a49e4d9601649b239a
Meruert <meru.forwork@gmail.com>
2024-01-31 22:52:11 +0500

    feat: complete getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -120,8 +120,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length > 0
+    ? +(arr.reduce((a, i) => a + i) / arr.length).toFixed(2)
+    : 0;
 }
 
 /**
