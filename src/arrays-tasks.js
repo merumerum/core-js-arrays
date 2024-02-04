f99e45a0dba8402d9f0130d493d69e9e6ea44242
Meruert <meru.forwork@gmail.com>
2024-02-04 17:46:30 +0500

    feat: complete createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -261,8 +261,8 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  return n < 1 ? 0 : new Array(size).fill(createNDimensionalArray(n - 1, size));
 }
 
 /**
