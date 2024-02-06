310a87c75a72822c81a484bc48704d27360431c7
Meruert <meru.forwork@gmail.com>
2024-02-06 10:52:34 +0500

    feat: complete getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -429,8 +429,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return Array.from(
+    numbers.filter((el) => el % 2 !== 0),
+    (x) => numbers.indexOf(x)
+  );
 }
 
 /**
